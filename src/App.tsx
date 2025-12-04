d8ed8825a57a90d2fe45c719b2299f8cd6b89bb1
vasantharaja <vasantharaja200295@gmail.com>
2025-12-04 21:11:29 +0530

    Feat(Core): Added Zustand, React Flow

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <h1>Hello world!</h1>
+      <h2>H2</h2>
+      <h3>h3</h3>
+      <h4>h4</h4>
+      <h5>h5</h5>
+      <h6>h6</h6>
+      <p>paragraph</p>
       <Button>Hello</Button>
     </div>
   );
