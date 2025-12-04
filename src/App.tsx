3e0afee85bcbf840b0917cb37e1382dc8e43621a
vasantharaja <vasantharaja200295@gmail.com>
2025-12-04 18:39:20 +0530

    Feat(Core): Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
